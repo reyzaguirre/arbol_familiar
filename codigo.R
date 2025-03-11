b06cac3bb642cb49b24f87db9813c0164a0f8ca4
Raul Eyzaguirre <reyzaguirre@gmail.com>
2025-03-11 09:04:09 -0500

    update

--- codigo.R
+++ codigo.R
@@ -9,9 +9,19 @@ library(openxlsx)
 
 d <- read.xlsx('entradas.xlsx')
 
-d$padre <- gsub('Eyzaguirre', 'E', d$padre)
-d$madre <- gsub('Eyzaguirre', 'E', d$madre)
-d$hijo <- gsub('Eyzaguirre', 'E', d$hijo)
+d$n <- nchar(d$hijo)
+d[d$n > 35, ]
+
+for (i in 1:3) {
+  d[, i] <- gsub('José de la Resurrección Eyzaguirre Butrón', 'José Eyzaguirre Butrón', d[, i])
+  d[, i] <- gsub('Eleodoro Eulogio Guillermo Eyzaguirre Liendo', 'Eleodoro Eyzaguirre Liendo', d[, i])
+  d[, i] <- gsub('Alondra Noelia Eyzaguirre De La Cruz', 'Alondra Eyzaguirre De La Cruz', d[, i])
+}
+
+# Reducir Eyzaguirre
+
+for (i in 1:3)
+  d[, i] <- gsub('Eyzaguirre', 'E', d[, i])
 
 # subgrupo
 
@@ -23,6 +33,18 @@ ped <- pedigree(
   momid = tmp$madre,
   sex = tmp$sex)
 
-png('arboles/butron_liendo.png', 9200, 2000, res = 150)
-plot(ped)
+png('arboles/butron_liendo.png', 9400, 2000, res = 150)
+plot(ped, mar = c(2, 5, 2, 5))
+dev.off()
+
+# full
+
+ped <- pedigree(
+  id = d$hijo, 
+  dadid = d$padre, 
+  momid = d$madre,
+  sex = d$sex)
+
+png('arboles/eyzaguirre.png', 10000, 2000, res = 150)
+plot(ped, mar = c(2, 5, 2, 5))
 dev.off()
